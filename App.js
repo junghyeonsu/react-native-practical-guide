d5beee1fef24b5c6dd459fe5f93f8ca217626cb9
junghyeonsu <jung660317@naver.com>
2020-12-30 15:23:20 +0900

    app.js 수정
    TextInput, Button Tag 추가
    StyleSheet 추가

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+          placeholder="추가 할 내용을 입력해주세요." 
+          style={styles.textInput}
+        />
+        <Button title="추가" />
+      </View>
+      <View>
+        <Text>...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-evenly', 
+    alignItems: 'center'
+  },
+  textInput: {
+    borderColor: 'black',
+    borderBottomWidth: 1,
+    padding: 10,
+    width: '80%'
+  }
 });
