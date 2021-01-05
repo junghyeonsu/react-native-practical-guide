4583e84098657e63bfcfa5fe26920c9a58b7d063
junghyeonsu <jung660317@naver.com>
2021-01-05 14:24:25 +0900

    76강
    이미지 스타일링
    - 이미지 둥글게 표현하기

--- rn-number-game/screens/GameOverScreen.js
+++ rn-number-game/screens/GameOverScreen.js
@@ -7,7 +7,13 @@ const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
             <Text>게임이 끝났습니다.</Text>
-            <Image source={require('../assets/success.png')} />
+            <View style={styles.imageContainer}>
+                <Image 
+                    source={require('../assets/success.png')} 
+                    style={styles.image}
+                    resizeMode="cover"
+                />
+            </View>
             <Text>시도 횟수 : {props.roundsNumber}</Text>
             <Text>숫자 : {props.userNumber}</Text>
             <Button title="새 게임" onPress={props.onRestart} />
@@ -20,6 +26,18 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    image : {
+        width: '100%',
+        height: '100%'
+    },
+    imageContainer : {
+        width: 300,
+        height: 300,
+        borderRadius: 200,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden'
     }
 })
 
