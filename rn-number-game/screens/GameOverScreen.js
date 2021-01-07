926d6b052dfc2ebbc2f8433c6d7ccde71e82174d
junghyeonsu <jung660317@naver.com>
2021-01-07 11:28:32 +0900

    94강
    동적으로 크기계산
    - 앞에 했었던 강의들과 비슷하게 Dimensions API를 이용해서 크기를 계산

--- rn-number-game/screens/GameOverScreen.js
+++ rn-number-game/screens/GameOverScreen.js
@@ -1,27 +1,29 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button, Image } from 'react-native'
+import { ScrollView ,StyleSheet, Text, View, Button, Image, Dimensions } from 'react-native'
 
 import Color from '../constants/colors'
 import MainButton from '../components/MainButton'
 
 const GameOverScreen = props => {
     return (
-        <View style={styles.screen}>
-            <Text>게임이 끝났습니다.</Text>
-            <View style={styles.imageContainer}>
-                <Image 
-                    // source={require('../assets/success.png')} 
-                    source={{uri: 'https://pds.joins.com//news/component/htmlphoto_mmdata/201808/03/6a4290a6-6421-414c-9df0-e09b03ce42a0.jpg'}}
-                    style={styles.image}
-                    resizeMode="cover"
-                />
+        <ScrollView>
+            <View style={styles.screen}>
+                <Text>게임이 끝났습니다.</Text>
+                <View style={styles.imageContainer}>
+                    <Image 
+                        // source={require('../assets/success.png')} 
+                        source={{uri: 'https://pds.joins.com//news/component/htmlphoto_mmdata/201808/03/6a4290a6-6421-414c-9df0-e09b03ce42a0.jpg'}}
+                        style={styles.image}
+                        resizeMode="cover"
+                    />
+                </View>
+                <Text>시도 횟수 : <Text style={styles.highlight}>{props.roundsNumber}</Text></Text>
+                <Text>숫자 : <Text style={styles.highlight}>{props.userNumber}</Text></Text>
+                <MainButton onPress={props.onRestart}>
+                    새 게임
+                </MainButton> 
             </View>
-            <Text>시도 횟수 : <Text style={styles.highlight}>{props.roundsNumber}</Text></Text>
-            <Text>숫자 : <Text style={styles.highlight}>{props.userNumber}</Text></Text>
-            <MainButton onPress={props.onRestart}>
-                새 게임
-            </MainButton> 
-        </View>
+        </ScrollView>
     )
 }
 
@@ -36,12 +38,17 @@ const styles = StyleSheet.create({
         height: '100%'
     },
     imageContainer : {
-        width: 300,
-        height: 300,
-        borderRadius: 200,
+        width: Dimensions.get('window').width * 0.7,
+        height: Dimensions.get('window').width * 0.7,
+        borderRadius: Dimensions.get('window').width * 0.7 / 2,
         borderWidth: 3,
         borderColor: 'black',
-        overflow: 'hidden'
+        overflow: 'hidden',
+        marginVertical: Dimensions.get('window').height / 30
+    },
+    resultContainer: {
+        marginHorizontal: 30,
+        marginVertical: Dimensions.get('window').height / 60
     },
     highlight : {
         color: Color.primary
