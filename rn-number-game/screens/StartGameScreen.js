f5ec500dd9750c298379eb7e7d76287aeb8a0535
junghyeonsu <jung660317@naver.com>
2021-01-07 11:51:29 +0900

    97강
    기기한테서 뷰 모드 변경 이벤트 듣기
    - Dimensions.get('window').width로 width를 계산하면, 문제점이 가로모드/세로모드를 변경했을 때 처음 width가 고정이 되서 view가 이상해진다.
    - 그래서 가로모드 / 세로모드 변경될 때마다 다시 Dimensions API를 새로 불러와서 width를 새로 계산하는 방식을 사용한다.
    - useEffect를 사용하면 조금 더 섹시하게 코딩할 수 있다.

--- rn-number-game/screens/StartGameScreen.js
+++ rn-number-game/screens/StartGameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { 
     StyleSheet, 
     Text, 
@@ -23,7 +23,8 @@ const StartGameScreen = (props) => {
     const [enteredValue, setEnteredValue] = useState('');
     const [confirmed, setConfirmed] = useState(false);
     const [selectedNumber, setSelectedNumber] = useState();
-
+    const [buttonWidth, setButtonWidth ] = useState(Dimensions.get('window').width / 4);
+    
     const numberInputHandler = inputText => {
         setEnteredValue(inputText.replace(/[^0-9]/g, ''));
     };
@@ -32,6 +33,16 @@ const StartGameScreen = (props) => {
         setEnteredValue('');
         setConfirmed(false);
     }
+
+    useEffect(() => {
+        const updateLayout = () => {
+            setButtonWidth(Dimensions.get('window').width / 4);
+        };
+        Dimensions.addEventListener('change', updateLayout);
+        return () => {
+            Dimensions.addEventListener('change', updateLayout);
+        }
+    })
 
     const confirmInputHandler = () => {
         const chosenNumber = parseInt(enteredValue);
@@ -84,14 +95,14 @@ const StartGameScreen = (props) => {
                                 onChangeText={numberInputHandler}
                                 value={enteredValue}
                             />
-                            <View style={styles.buttonContainer}>
+                            <View style={{width:buttonWidth}}>
                                 <View style={styles.button}>
                                     <Button title="초기화" 
                                         onPress={resetInputHandler} 
                                         color={Colors.accent} 
                                     />
                                 </View>
-                                <View style={styles.button}>
+                                <View style={{width:buttonWidth}}>
                                     <Button 
                                         title="확인" 
                                         onPress={confirmInputHandler} 
